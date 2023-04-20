aadf8a2a55b418404bfc770b8fde2ea8f62f62e8
Philipp Mandler <info@philipp-mandler.com>
2023-04-21 01:24:08 +0200

    Format

--- src/apply/text.ts
+++ src/apply/text.ts
@@ -1,35 +1,35 @@
-import { InsertTextOperation, RemoveTextOperation } from 'slate'
+import { InsertTextOperation, RemoveTextOperation } from "slate";
 
-import { getTargetFromDoc } from '../path'
-import { SyncValue } from '../model'
+import { getTargetFromDoc } from "../path";
+import { SyncValue } from "../model";
 
 export const insertText = (
   doc: SyncValue,
   op: InsertTextOperation
 ): SyncValue => {
-  const node = getTargetFromDoc(doc, op.path)
+  const node = getTargetFromDoc(doc, op.path);
 
-  const offset = Math.min(node.text.length, op.offset)
+  const offset = Math.min(node.text.length, op.offset);
 
-  node.text.insertAt(offset, ...op.text)
+  node.text.insertAt(offset, ...op.text);
 
-  return doc
-}
+  return doc;
+};
 
 export const removeText = (
   doc: SyncValue,
   op: RemoveTextOperation
 ): SyncValue => {
-  const node = getTargetFromDoc(doc, op.path)
+  const node = getTargetFromDoc(doc, op.path);
 
-  const offset = Math.min(node.text.length, op.offset)
+  const offset = Math.min(node.text.length, op.offset);
 
-  node.text.deleteAt(offset, op.text.length)
+  node.text.deleteAt(offset, op.text.length);
 
-  return doc
-}
+  return doc;
+};
 
 export default {
   insert_text: insertText,
-  remove_text: removeText
-}
+  remove_text: removeText,
+};
