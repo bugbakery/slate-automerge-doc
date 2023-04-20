aed1052c906db35c898b69d7e315ab62d43f14fc
Philipp Mandler <info@philipp-mandler.com>
2023-04-21 01:23:14 +0200

    Add build script

---
+++ build.js
@@ -0,0 +1,25 @@
+const { build } = require('esbuild');
+const glob = require('fast-glob');
+const { Project } = require('ts-morph');
+
+async function run() {
+  const outDir = 'lib';
+  const files = await glob('src/**/*.ts');
+
+  const tsProject = new Project({
+    compilerOptions: { outDir, emitDeclarationOnly: true },
+    tsConfigFilePath: './tsconfig.json',
+    skipAddingFilesFromTsConfig: true,
+  });
+  tsProject.addSourceFilesAtPaths(files);
+  await tsProject.emit();
+
+  await build({
+    entryPoints: files,
+    outdir: outDir,
+    format: 'cjs',
+    sourcemap: true,
+  });
+}
+
+run();
