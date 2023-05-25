f25c5b9902c5b83d9b5983a845bab7ca94c4f095
Philipp Mandler <info@philipp-mandler.com>
2023-05-25 19:36:46 +0200

    Add watch mode

--- build.js
+++ build.js
@@ -1,25 +1,52 @@
-const { build } = require('esbuild');
-const glob = require('fast-glob');
-const { Project } = require('ts-morph');
+const chokidar = require("chokidar");
+const esbuild = require("esbuild");
+const glob = require("fast-glob");
+const { Project } = require("ts-morph");
 
-async function run() {
-  const outDir = 'lib';
-  const files = await glob('src/**/*.ts');
+async function build(filePattern) {
+  const outDir = "lib";
+  const files = await glob(filePattern);
 
   const tsProject = new Project({
     compilerOptions: { outDir, emitDeclarationOnly: true },
-    tsConfigFilePath: './tsconfig.json',
+    tsConfigFilePath: "./tsconfig.json",
     skipAddingFilesFromTsConfig: true,
   });
   tsProject.addSourceFilesAtPaths(files);
   await tsProject.emit();
 
-  await build({
+  await esbuild.build({
     entryPoints: files,
     outdir: outDir,
-    format: 'esm',
+    format: "esm",
     sourcemap: true,
   });
+}
+
+function watch(filePattern) {
+  chokidar.watch(filePattern).on("change", async (file) => {
+    try {
+      console.log("file changed:", file);
+      await build(filePattern);
+    } catch {
+      // nevermind, errors are logged by esbuild
+    }
+  });
+}
+
+async function run() {
+  const watchMode = process.argv.includes("--watch");
+  const filePattern = "src/**/*.ts";
+
+  await build(filePattern).catch(() => {
+    if (!watchMode) {
+      process.exit(1);
+    }
+  });
+
+  if (watchMode) {
+    watch(filePattern);
+  }
 }
 
 run();
